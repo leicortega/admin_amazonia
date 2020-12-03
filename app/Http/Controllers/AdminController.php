20aa576e496c1b537c4e7da076bfc42610bb9cb8
Leiner Ortega <leicortega@gmail.com>
2020-12-03 11:37:21 -0500

    Rutas de departamentos y municipios con metodo GET

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,9 +20,9 @@ use App\User;
 
 class AdminController extends Controller
 {
-    public function __construct() {
-        $this->middleware('auth');
-    }
+    // public function __construct() {
+    //     $this->middleware('auth');
+    // }
 
     public function users() {
         $users = User::paginate(10);
